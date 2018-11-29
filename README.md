16d74b7148f3fe12d02472359dfcbb0c16291ed2
MDTrue <MDTrueII@hotmail.com>
2018-11-28 22:25:29 -0800

    readme added and a few bugs fixed

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Train-Activity
+#Train activity
+=======
+
+## utilize the power of firebase to create a train time table
+
+an updatable train schedule with a working clock and calculations for arrival times.
+format is able to retain integrity at any size screen.
+
+![Game Image](assets/images/BigCityRailway.png)
+
+---
+
+Made with:
+
+  * html
+  * css
+  * bootstrap
+  * javascript
+  * jquery
+  * momentjs
+  * firebase
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,4 @@
 // console.log("hello")
-//declaring the firebase variable
-var database = firebase.database();
-
 //using moment to add a clock to the web page
 var currentTime = function () {
     var clock = moment().format("hh:mm")
@@ -10,68 +7,83 @@ var currentTime = function () {
 $(document).ready(function () {
     currentTime();
     setInterval(currentTime, 1000)
-});
+//declaring the firebase variable
+  // Fill in this config file with the settings provided to you by Firebase
+  var config = {
+    apiKey: "AIzaSyAsweIWgRjUgFoHDdwwDwCL1qyPaKSzu84",
+    authDomain: "trainactivity-430a0.firebaseapp.com",
+    databaseURL: "https://trainactivity-430a0.firebaseio.com",
+    projectId: "trainactivity-430a0",
+    storageBucket: "trainactivity-430a0.appspot.com",
+    messagingSenderId: "93359047242"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
 
-//adding trains to the list
-$(document).on("click", ".submitButton", function (event) {
-    event.preventDefault();
+var database = firebase.database();
 
-    //declaring variables
-
-    var name = $("#nameInput").val().trim();
-    var destination = $("#destinationInput").val().trim();
-    var frequency = $("#frequencyInput").val().trim();
-    var arrival = $("#timeInput").val().trim();
-
-    //place holder variables for math
-    //convert start time to one year ago to ensure it is before current time. 
-    var startTimePast = moment(arrival, "hh:mm").subtract(1, "years");
-    // console.log(startTimePast);
-    // console.log(clock)
-    var timeDifference = moment().diff(moment(startTimePast), "minutes");
-    // console.log(timeDifference);
-    var timeRemainder = timeDifference % frequency;
-    // console.log(timeRemainder);
-    var timeTillNext = frequency - timeRemainder;
-    // console.log(timeTillNext)
-    var nextTrain = moment().add(timeTillNext, "minutes").format("hh:mm");
-    // console.log(nextTrain)
-
-    //store variables in firebase
-
-    database.ref().push({
-        name: name,
-        destination: destination,
-        arrival: arrival,
-        frequency: frequency,
-        timeTillNext: timeTillNext,
-        nextTrain: nextTrain,
-    })
-    // clear input boxes
-    $("#nameInput").val("")
-    $("#destinationInput").val("")
-    $("#frequencyInput").val("")
-    $("#timeInput").val("")
-
-
-});
-
-//load the train and all in firebase into the table
-database.ref().on("child_added", function (childSnapshot) {
-
-    // console.log("hello")
-    // console.log(childSnapshot)
-
-    var newRow = $("<tr>");
-    var tableName = $("<td>" + childSnapshot.val().name + "</td>");
-    var tableDestination = $("<td>" + childSnapshot.val().destination + "</td>");
-    var tableFrequency = $("<td>" + childSnapshot.val().frequency +"</td>");
-    var nextArrival = $("<td>" + childSnapshot.val().nextTrain + "</td>");
-    var minsAway = $("<td>" + childSnapshot.val().timeTillNext + "</td>")
-
-    //append each row separately, the variables just keep it neater and easier to edit
-    newRow.append(tableName, tableDestination, tableFrequency,nextArrival,minsAway)
-    $(".table").append(newRow)
+    
+    //adding trains to the list
+    $(document).on("click", ".submitButton", function (event) {
+        event.preventDefault();
+        
+        //declaring variables
+        
+        var name = $("#nameInput").val().trim();
+        var destination = $("#destinationInput").val().trim();
+        var frequency = $("#frequencyInput").val().trim();
+        var arrival = $("#timeInput").val().trim();
+        
+        //place holder variables for math
+        //convert start time to one year ago to ensure it is before current time. 
+        var startTimePast = moment(arrival, "hh:mm").subtract(1, "years");
+        // console.log(startTimePast);
+        // console.log(clock)
+        var timeDifference = moment().diff(moment(startTimePast), "minutes");
+        // console.log(timeDifference);
+        var timeRemainder = timeDifference % frequency;
+        // console.log(timeRemainder);
+        var timeTillNext = frequency - timeRemainder;
+        // console.log(timeTillNext)
+        var nextTrain = moment().add(timeTillNext, "minutes").format("hh:mm");
+        // console.log(nextTrain)
+        
+        //store variables in firebase
+        
+        database.ref().push({
+            name: name,
+            destination: destination,
+            arrival: arrival,
+            frequency: frequency,
+            timeTillNext: timeTillNext,
+            nextTrain: nextTrain,
+        })
+        
+        
+    });
+    
+    //load the train and all in firebase into the table
+    database.ref().on("child_added", function (childSnapshot) {
+        $("tbody").empty
+        // console.log("hello")
+        // console.log(childSnapshot)
+       
+        var newRow = $("<tr>");
+        var tableName = $("<td>" + childSnapshot.val().name + "</td>");
+        var tableDestination = $("<td>" + childSnapshot.val().destination + "</td>");
+        var tableFrequency = $("<td>" + childSnapshot.val().frequency +"</td>");
+        var nextArrival = $("<td>" + childSnapshot.val().nextTrain + "</td>");
+        var minsAway = $("<td>" + childSnapshot.val().timeTillNext + "</td>")
+        
+        //append each row separately, the variables just keep it neater and easier to edit
+        newRow.append(tableName, tableDestination, tableFrequency,nextArrival,minsAway)
+        $(".table").append(newRow)
+        // clear input boxes
+        $("#nameInput").val("")
+        $("#destinationInput").val("")
+        $("#frequencyInput").val("")
+        $("#timeInput").val("")
+    });
 })
 
 
